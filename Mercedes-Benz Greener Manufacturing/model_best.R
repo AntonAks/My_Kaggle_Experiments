c3da6d87d0189067b2399922a94d9a8fce87a567
AntonAks <antonaks84@gmail.com>
2017-06-22 23:56:21 +0300

    tuning...

--- Mercedes-Benz Greener Manufacturing/model_best.R
+++ Mercedes-Benz Greener Manufacturing/model_best.R
@@ -23,8 +23,16 @@ train_df_model$y <- NULL
 #Row binding train & test set for feature engineering
 train_test <- rbindlist(l = list(train_df_model, test_data),use.names = T)
 #ntrain <- nrow(train_df_model)
-
 features <- names(train_data)
+
+
+train_test <- train_test[,-c(378,377,376,375, 364, 359
+                             , 358, 357, 346, 332, 312
+                             , 303, 300, 281, 8, 9, 10, 11
+                             , 17, 19, 23, 26, 27, 29, 32
+                             , 33, 35, 48, 40, 52, 53, 58
+                             , 55, 71, 79, 70, 72, 74, 75)]
+
 
 #convert character into integer
 for (f in features) {
@@ -52,13 +60,15 @@ dtest <- xgb.DMatrix(as.matrix(test_x))
 ##xgboost parameters
 xgb_params <- list(booster = "gbtree" 
                    , objective = "reg:linear"
-                   , eta=0.01 # 0.005 - BEST
+                   , eta=0.005 # 0.005 - BEST
                    , gamma=0
-                   , max_depth=4 
-                   , subsample=0.95
+                   , max_depth=4 # 4 - BEST
+                   , subsample=0.7 # 0.95 - BEST
                    , min_child_weight = 3 # ??? try to comment
                    , base_score=mean(y_train)
 )
+
+
 
 #tuning
 xgbcv <- xgb.cv(params = xgb_params,
@@ -76,11 +86,12 @@ xgbcv <- xgb.cv(params = xgb_params,
 set.seed(12)
 gb_dt <- xgb.train(params = xgb_params
                    , data = dtrain
-                   , nrounds = 380
+                   , nrounds = 769
                    , print_every_n = 10
                    , early_stopping_rounds = 10
                    , maximize = F
                    , watchlist = list(train=dtrain))
+
 
 
 
@@ -94,7 +105,6 @@ check_res[,check:= abs(y - y_pred) / y]
 e <- mean(check_res$check)
 
 write_csv(x = result, 'Mercedes-Benz Greener Manufacturing/final_submission.csv')
-
 
 
 
